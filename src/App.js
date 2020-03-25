59f540e3bec217e89b2ee08b7baa0609d874190b
Barry Grubb <bgrubb83@gmail.com>
2020-03-25 13:51:34 +0000

    Add various test states to App component, return result from helper file, set it to state and render it.

--- src/App.js
+++ src/App.js
@@ -11,10 +11,24 @@ class App extends React.Component {
   }
 
   async componentDidMount() {
-    getProcessingPage('test');
+    /* Various states to be passed to the helper function */
+    /* Uncomment as neceesary to try all outcomes */
+
+    let stateObj;
+
+    // stateObj = [{ state: 'processing' }, { state: 'success' }];
+    // stateObj = [{ state: 'processing' }, { state: 'error' }];
+    // stateObj = [{ state: 'processing' }, { state: 'error', errorCode: 'NO_STOCK' }];
+    // stateObj = [{ state: 'processing' }, { state: 'error', errorCode: 'INCORRECT_DETAILS' }];
+    // stateObj = [{ state: 'processing' }, { state: 'error', errorCode: null }];
+    stateObj = [{ state: 'processing' }, { state: 'error', errorCode: undefined }];
+
+    const result = getProcessingPage(stateObj);
+    this.setState({ result });
   }
 
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <p>{this.state.result}</p>
--- src/helpers/processingHelpers.js
+++ src/helpers/processingHelpers.js
@@ -1,3 +1,9 @@
+/*
+ * Gets the processing page
+ * @param {array} data 
+ */
+
 export function getProcessingPage(arg) {
     console.log('called with', arg);
+    return 'Fake result';
 }
