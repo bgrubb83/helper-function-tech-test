497fec9b1cd7aa132491b2d39ab83d2c0ccd769c
Barry Grubb <bgrubb83@gmail.com>
2020-03-25 14:09:48 +0000

    Render argument to screen for easiler comparison against the result.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      argument: '',
       result: 'Processing...',
     };
   }
@@ -23,6 +24,8 @@ class App extends React.Component {
     // stateObj = [{ state: 'processing' }, { state: 'error', errorCode: null }];
     // stateObj = [{ state: 'processing' }, { state: 'error', errorCode: undefined }];
 
+    this.setState({ argument: JSON.stringify(stateObj) });
+
     const result = await getProcessingPage(stateObj);
     this.setState({ result: JSON.stringify(result) });
   }
@@ -31,7 +34,8 @@ class App extends React.Component {
     console.log('state', this.state);
     return (
       <div className="App">
-        <p>{this.state.result}</p>
+        <p>Argument: {this.state.argument}</p>
+        <p>Result: {this.state.result}</p>
       </div>
     );
   }
